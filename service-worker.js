a928ba883d369469d882e2ba5376837ab9198fdb
milak-web <milak-web@users.noreply.github.com>
2026-05-02 20:04:15 +0200

    Improve post-boss pressure zoom and reduce spawn-loop overhead

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "dragonborn-pyromancer-v17";
+﻿const CACHE_NAME = "dragonborn-pyromancer-v18";
 const ASSETS = [
   "./",
   "./index_enemy_boss_escalation_rebuild.html",
@@ -58,6 +58,7 @@ self.addEventListener("fetch", event => {
     )
   );
 });
+
 
 
 
